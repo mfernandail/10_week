ef94a3ac7560ed6d3bf123255bf9ad1e33dd4b7d
mfernandail <maria.ibanez.leiva@gmail.com>
2025-06-28 22:35:28 -0400

    Add simple script

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-class Expense {
-  constructor(description, amount, category, date = new Date()) {
-    this.description = description
-    this.amount = parseFloat(amount)
-    this.category = category
-    this.date = date.toLocaleDateString()
-  }
+const btn_submit = document.querySelector('#btn_submit')
+const form = document.querySelector('#expenseForm')
+
+eventListener()
+
+function eventListener() {
+  btn_submit.addEventListener('click', addSpent)
+}
+
+function addSpent(e) {
+  e.preventDefault()
+
+  form.reset()
+  console.log('hola')
 }
